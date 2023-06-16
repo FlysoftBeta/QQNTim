78f8f0650678e83895c40b52f5441a54a05584bc
Flysoft <1922656933@qq.com>
2023-06-17 00:07:17 +0800

    feat(examples/auto-reply): add example plugin

---
+++ examples/自动回复/renderer.js
@@ -0,0 +1,15 @@
+module.exports = (qqntim) => {
+    qqntim.nt.on("new-messages", (messages) => {
+        console.log("[Example-AutoReply] 收到新消息", messages);
+        messages.forEach((message) => {
+            if (message.chatType != "friend") return;
+            qqntim.nt.sendMessage("friend", message.peer.uid, [
+                {
+                    type: "text",
+                    content: "收到一条来自好友的消息：",
+                },
+                ...message.elements,
+            ]);
+        });
+    });
+};
