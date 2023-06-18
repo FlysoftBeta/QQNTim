34b6e39759641b5bc4dea71252216d9ca7f35cc9
Flysoft <1922656933@qq.com>
2023-06-18 20:32:42 +0800

    feat(examples/auto-reply): add example for `getPreviousMessages`

--- examples/自动回复/renderer.js
+++ examples/自动回复/renderer.js
@@ -1,10 +1,34 @@
 module.exports = (qqntim) => {
     qqntim.nt.getFriendsList().then((list) => {
         console.log("[Example-AutoReply] 好友列表", list);
+        list.forEach((friend) =>
+            qqntim.nt
+                .getPreviousMessages({ chatType: "friend", uid: friend.uid }, 20)
+                .then((messages) =>
+                    console.log(
+                        "[Example-AutoReply] 好友 " +
+                            friend.nickName +
+                            " 的最近 20 条消息",
+                        messages
+                    )
+                )
+        );
     });
+
     qqntim.nt.getGroupsList().then((list) => {
         console.log("[Example-AutoReply] 群组列表", list);
+        list.forEach((group) =>
+            qqntim.nt
+                .getPreviousMessages({ chatType: "group", uid: group.uid }, 20)
+                .then((messages) =>
+                    console.log(
+                        "[Example-AutoReply] 群组 " + group.name + " 的最近 20 条消息",
+                        messages
+                    )
+                )
+        );
     });
+
     qqntim.nt.on("new-messages", (messages) => {
         console.log("[Example-AutoReply] 收到新消息", messages);
         messages.forEach((message) => {
