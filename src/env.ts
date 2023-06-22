ed2906b7b0e2731637704c50372ed2081e137a5b
Flysoft <1922656933@qq.com>
2023-06-22 19:51:51 +0800

    feat(main/plugins): allow changing home directory, closes https://github.com/Flysoft-Studio/QQNTim/issues/35 (#39)

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,13 @@
+import * as path from "path";
+
+const s = path.sep;
+
 export const verboseLogging = !!process.env["QQNTIM_VERBOSE_LOGGING"];
+export const dataDir = path.resolve(
+    process.env["QQNTIM_HOME"] ||
+        (process.platform == "win32"
+            ? `${process.env["UserProfile"]}${s}.qqntim`
+            : `${process.env["HOME"]}${s}.local${s}share${s}QQNTim`)
+);
+export const configFile = `${dataDir}${s}config.json`;
+export const pluginDir = `${dataDir}${s}plugins`;
