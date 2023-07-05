027a7c9904e820cbc6009fb19cae282f9abab50d
Flysoft <flysoftbeta@outlook.com>
2023-07-05 23:23:41 +0800

    refactor(all/loader): extract `getUserPlugins` logic

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as fs from "fs-extra";
 import {
+    AllUsersPlugins,
     LoadedPlugins,
     Plugin,
     PluginInjection,
@@ -42,11 +43,26 @@ export function applyStylesheets() {
     document.body.appendChild(element);
 }
 
+function getUserPlugins(allPlugins: AllUsersPlugins, uin: string) {
+    const userPlugins = allPlugins[uin];
+    if (!userPlugins) {
+        console.warn(`[!Loader] 账户 (${uin}) 没有插件，跳过加载`);
+        return;
+    }
+    if (uin != "") console.log(`[!Loader] 正在为账户 (${uin}) 加载插件`);
+    else console.log(`[!Loader] 正在为所有账户加载插件`);
+    return userPlugins;
+}
+
 export function loadPlugins(
-    userPlugins: UserPlugins,
+    allPlugins: AllUsersPlugins,
+    uin: string,
     shouldInject: (injection: PluginInjection) => boolean,
     injectStylesheets: boolean
 ) {
+    const userPlugins = getUserPlugins(allPlugins, uin);
+    if (!userPlugins) return false;
+
     for (const id in userPlugins) {
         if (loadedPlugins[id]) continue;
         const plugin = userPlugins[id];
@@ -67,4 +83,6 @@ export function loadPlugins(
                 scripts.push([plugin, `${plugin.dir}${s}${injection.script}`]);
         });
     }
+
+    return true;
 }
--- src/renderer/loader.ts
+++ src/renderer/loader.ts
@@ -33,18 +33,10 @@ function shouldInject(injection: PluginInjection, page: Page) {
 }
 
 export function applyPlugins(allPlugins: AllUsersPlugins, uin: string = "") {
-    const userPlugins = allPlugins[uin];
-    if (!userPlugins) {
-        console.warn(`[!Loader] 账户 (${uin}) 没有插件，跳过加载`);
-        return false;
-    }
-    if (uin != "") console.log(`[!Loader] 正在为账户 (${uin}) 加载插件`);
-    else console.log(`[!Loader] 正在为所有账户加载插件`);
-
     const page = detectCurrentPage();
     if (page == "about") return false;
 
-    loadPlugins(userPlugins, (injection) => shouldInject(injection, page), true);
+    loadPlugins(allPlugins, uin, (injection) => shouldInject(injection, page), true);
     applyScripts(api);
     windowLoadPromise.then(() => applyStylesheets());
 
