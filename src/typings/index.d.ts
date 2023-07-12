75006eb3594a167e60b0b77c8f2aff4b7902ed80
Flysoft <flysoftbeta@outlook.com>
2023-07-12 13:54:16 +0800

    docs: detailed user guide and development guide, closes https://github.com/Flysoft-Studio/QQNTim/issues/91 (#128)

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -9,9 +9,21 @@ declare namespace QQNTim {
         type Request = any[];
         type Args<T> = [{ type: string; eventName: string; callbackId: string }, T];
         interface InterruptIPCOptions {
+            /**
+             * 类型 (`request` 或 `response`)
+             */
             type?: Type;
+            /**
+             * 事件名称 (如 `ns-ntApi` 或 `ns-fsApi` 等)
+             */
             eventName?: string;
+            /**
+             * 命令名称
+             */
             cmdName?: string;
+            /**
+             * 方向 (接收或发送)
+             */
             direction?: Direction | undefined;
         }
         type InterruptFunction = (args: Args<any>, channel: string, sender?: Electron.WebContents) => boolean | void;
@@ -25,20 +37,50 @@ declare namespace QQNTim {
     namespace Configuration {
         type Configuration = {
             plugins?: {
+                /**
+                 * 插件白名单
+                 */
                 whitelist?: string[];
+                /**
+                 * 插件黑名单
+                 */
                 blacklist?: string[];
             };
+            /**
+             * 显示详细日志输出
+             * @description 开启后，可以在控制台内查看到 IPC 通信、部分 Electron 对象的成员访问信息等。
+             */
             verboseLogging?: boolean;
+            /**
+             * 使用原版 DevTools
+             * @description 使用 Chromium DevTools 而不是 chii DevTools (Windows 版 9.8.5 及以上不可用)。
+             */
             useNativeDevTools?: boolean;
+            /**
+             * 禁用兼容性处理
+             * @description 禁用后，LiteLoader 和 BetterQQNT 可能将不能与 QQNTim 一起使用。
+             */
             disableCompatibilityProcessing?: boolean;
         };
 
         interface Environment {
             config: Required<Configuration>;
             path: {
+                /**
+                 * 数据目录
+                 */
                 dataDir: string;
+                /**
+                 * 配置文件 (`qqntim.json`)
+                 */
                 configFile: string;
+                /**
+                 * 插件目录 (`plugins`)
+                 */
                 pluginDir: string;
+                /**
+                 * 用户插件目录 (`plugins-user`)
+                 */
                 pluginPerUserDir: string;
             };
         }
@@ -58,6 +100,9 @@ declare namespace QQNTim {
              * @param qqntim QQNTim API
              */
             constructor(qqntim: API.Renderer.API);
+            /**
+             * 当页面加载完毕时触发
+             */
             onWindowLoaded?(): void;
         }
     }
@@ -76,12 +121,33 @@ declare namespace QQNTim {
         }
         type Injection = InjectionMain | InjectionRenderer;
         interface Plugin {
+            /**
+             * 是否已经加载
+             */
             loaded: boolean;
+            /**
+             * 当前环境是否满足条件
+             */
             meetRequirements: boolean;
+            /**
+             * 是否启用
+             */
             enabled: boolean;
+            /**
+             * 唯一 ID
+             */
             id: string;
+            /**
+             * 插件所在目录
+             */
             dir: string;
+            /**
+             * 脚本和样式注入
+             */
             injections: Injection[];
+            /**
+             * 插件清单
+             */
             manifest: Manifest.Manifest;
         }
         type AllUsersPlugins = Record<string, UserPlugins>;
@@ -92,8 +158,17 @@ declare namespace QQNTim {
     namespace API {
         namespace Main {
             interface API {
+                /**
+                 * 当前的配置数据和配置文件路径
+                 */
                 env: Configuration.Environment;
+                /**
+                 * QQNTim 版本号
+                 */
                 version: string;
+                /**
+                 * QQNT 版本号
+                 */
                 ntVersion: string;
                 interrupt: {
                     /**
@@ -123,9 +198,21 @@ declare namespace QQNTim {
         }
         namespace Renderer {
             interface API {
+                /**
+                 * 所有已经扫描到的插件列表
+                 */
                 allPlugins: Plugin.AllUsersPlugins;
+                /**
+                 * 当前的配置数据和配置文件路径
+                 */
                 env: Configuration.Environment;
+                /**
+                 * QQNTim 版本号
+                 */
                 version: string;
+                /**
+                 * QQNT 版本号
+                 */
                 ntVersion: string;
                 interrupt: {
                     /**
@@ -136,39 +223,104 @@ declare namespace QQNTim {
                      */
                     ipc(func: IPC.InterruptFunction, options: IPC.InterruptIPCOptions): void;
                 };
+                /**
+                 * NT API
+                 */
                 nt: NT.NT;
+                /**
+                 * 窗口 API
+                 */
                 browserWindow: BrowserWindowAPI;
+                /**
+                 * 应用程序生命周期 API
+                 */
                 app: AppAPI;
+                /**
+                 * 对话框 API
+                 */
                 dialog: DialogAPI;
                 modules: {
                     fs: typeof import("fs-extra");
                 };
                 utils: {
+                    /**
+                     * 等待 DOM 元素出现
+                     * @description 当元素出现时，此函数返回的 Promise 会 resolve 一个元素。
+                     * @param selector CSS 选择器
+                     */
                     waitForElement<T extends Element>(selector: string): Promise<T>;
+                    /**
+                     * 调用 NT API
+                     * @param eventName 事件名称 (如 `ns-ntApi` 或 `ns-fsApi` 等)
+                     * @param cmd 命令名称
+                     * @param args 参数
+                     */
                     ntCall(eventName: string, cmd: string, args: any[]): Promise<IPC.Response>;
+                    /**
+                     * 拦截 NT API 调用
+                     * @param callback 回调函数
+                     * @param eventName 事件名称 (如 `ns-ntApi` 或 `ns-fsApi` 等)
+                     * @param cmdName 命令名称
+                     * @param direction 方向 (接收或发送)
+                     * @param type 类型 (`request` 或 `response`)
+                     */
                     ntInterrupt(callback: QQNTim.IPC.InterruptFunction, eventName: string, cmdName: string, direction?: QQNTim.IPC.Direction, type?: QQNTim.IPC.Type): void;
+                    /**
+                     * 获取与 DOM 元素相关联的 Vue 组件 ID
+                     * @param element 元素
+                     * @example
+                     * ```html
+                     * <div class="xxxxx" data-vxxxxxx>xxxxx</div>
+                     * ```
+                     * 将会返回 `data-vxxxxxx`。
+                     */
                     getVueId(element: HTMLElement): string | undefined;
                 };
+                /**
+                 * @description 当页面加载完毕时，此 Promise 会 resolve。
+                 */
                 windowLoadPromise: WindowLoadPromise;
             }
             type WindowLoadPromise = Promise<void>;
             namespace NT {
                 interface MessageElementBase {
+                    /**
+                     * QQNT 原始数据
+                     */
                     raw: object;
                 }
                 interface MessageElementText extends MessageElementBase {
                     type: "text";
+                    /**
+                     * 文字数据
+                     */
                     content: string;
                 }
                 interface MessageElementImage extends MessageElementBase {
                     type: "image";
+                    /**
+                     * 图片文件路径
+                     */
                     file: string;
+                    /**
+                     * @description 当此图片文件下载完成时此 Promise 会 resolve。
+                     */
                     downloadedPromise: Promise<void>;
                 }
                 interface MessageElementFace extends MessageElementBase {
                     type: "face";
+                    /**
+                     * 表情 ID
+                     */
                     faceIndex: number;
+                    /**
+                     * 表情类型
+                     * @description 分别为普通表情、扩展表情、超级表情。当此属性为一个数字时代表此类型未知。
+                     */
                     faceType: "normal" | "normal-extended" | "super" | number;
+                    /**
+                     * 超级表情 ID
+                     */
                     faceSuperIndex?: number;
                 }
                 interface MessageElementRaw extends MessageElementBase {
@@ -177,58 +329,198 @@ declare namespace QQNTim {
                 type MessageElement = MessageElementText | MessageElementImage | MessageElementFace | MessageElementRaw;
                 type MessageElementSend = Omit<MessageElementText, "raw"> | Omit<MessageElementImage, "raw"> | Omit<Omit<MessageElementFace, "raw">, "downloadPromise"> | MessageElementRaw;
                 interface Message {
+                    /**
+                     * 聊天会话
+                     */
                     peer: Peer;
+                    /**
+                     * 消息发送者
+                     */
                     sender: Sender;
+                    /**
+                     * 消息元素
+                     */
                     elements: MessageElement[];
+                    /**
+                     * @description 当消息内所有媒体文件下载完成时此 Promise 会 resolve。
+                     */
                     allDownloadedPromise: Promise<void[]>;
+                    /**
+                     * QQNT 原始数据
+                     */
                     raw: object;
                 }
                 interface Sender {
+                    /**
+                     * 发送者用户 UID
+                     */
                     uid: string;
+                    /**
+                     * 发送者群昵称
+                     * 只有在群聊中，此属性才不为空。
+                     */
                     memberName?: string;
+                    /**
+                     * 发送者昵称
+                     */
                     nickName?: string;
                 }
                 interface Peer {
+                    /**
+                     * 聊天会话类型
+                     */
                     chatType: "friend" | "group" | "others";
+                    /**
+                     * 对象用户 ID
+                     */
                     uid: string;
+                    /**
+                     * 聊天会话名称
+                     * 主动提供此对象时，不需要设置此属性。
+                     */
                     name?: string;
                 }
                 interface User {
+                    /**
+                     * 用户 ID
+                     */
                     uid: string;
+                    /**
+                     * QQ 号
+                     */
                     uin: string;
+                    /**
+                     * QID
+                     */
                     qid: string;
+                    /**
+                     * 群头像 URL
+                     */
                     avatarUrl: string;
+                    /**
+                     * 昵称
+                     */
                     nickName: string;
+                    /**
+                     * 个性签名
+                     */
                     bio: string;
+                    /**
+                     * 性别
+                     */
                     sex: "male" | "female" | "unset" | "others";
+                    /**
+                     * QQNT 原始数据
+                     */
                     raw: object;
                 }
                 interface Group {
+                    /**
+                     * 用户 ID
+                     */
                     uid: string;
+                    /**
+                     * 群头像 URL
+                     */
                     avatarUrl: string;
+                    /**
+                     * 群名称
+                     */
                     name: string;
+                    /**
+                     * 身份组
+                     */
                     role: "master" | "moderator" | "member" | "others";
+                    /**
+                     * 成员数量上限
+                     */
                     maxMembers: number;
+                    /**
+                     * 成员数量
+                     */
                     members: number;
+                    /**
+                     * QQNT 原始数据
+                     */
                     raw: object;
                 }
                 interface LoginAccount {
+                    /**
+                     * 用户 ID
+                     */
                     uid: string;
+                    /**
+                     * QQ 号
+                     */
                     uin: string;
                 }
 
                 type Events = {
+                    /**
+                     * 当收到新消息时触发
+                     * @param messages 新消息
+                     * @returns
+                     */
                     "new-messages": (messages: Message[]) => void;
+                    /**
+                     * 当好友列表更新时触发
+                     * @param list 用户列表
+                     * @returns
+                     */
                     "friends-list-updated": (list: User[]) => void;
+                    /**
+                     * 当群列表更新时触发
+                     * @param list 群列表
+                     * @returns
+                     */
                     "groups-list-updated": (list: Group[]) => void;
                 };
                 type EventEmitter = TypedEmitter<Events>;
                 interface NT extends EventEmitter {
+                    /**
+                     * 获取当前登录的账号
+                     * @returns 当前登录的账号
+                     */
                     getAccountInfo(): Promise<LoginAccount | undefined>;
+                    /**
+                     * 获取用户信息
+                     * @param uid 用户 ID
+                     * @returns 用户信息
+                     */
+                    getUserInfo(uid: string): Promise<User>;
+                    /**
+                     * 撤回一条消息
+                     * @param peer 聊天会话
+                     * @param message 消息 ID
+                     * @returns
+                     */
                     revokeMessage(peer: Peer, message: string): Promise<void>;
+                    /**
+                     * 发送一条消息
+                     * @param peer 聊天会话
+                     * @param elements 消息元素
+                     * @returns 消息 ID
+                     */
                     sendMessage(peer: Peer, elements: MessageElement[]): Promise<string>;
+                    /**
+                     * 获取好友列表
+                     * @param forced 忽略缓存强制获取
+                     * @returns 用户信息列表
+                     */
                     getFriendsList(forced: boolean): Promise<User[]>;
+                    /**
+                     * 获取群列表
+                     * @param forced 忽略缓存强制获取
+                     * @returns 群列表
+                     */
                     getGroupsList(forced: boolean): Promise<Group[]>;
+                    /**
+                     * 获取历史聊天记录
+                     * @param peer 聊天会话
+                     * @param count 消息数量
+                     * @param startMsgId 开始位置 (消息 ID)
+                     * @returns 历史消息列表
+                     */
                     getPreviousMessages(peer: Peer, count?: number, startMsgId?: string): Promise<Message[]>;
                 }
             }
@@ -238,15 +530,49 @@ declare namespace QQNTim {
                 setMinimumSize(width: number, height: number): void;
             }
             interface AppAPI {
+                /**
+                 * 重启
+                 */
                 relaunch(): void;
+                /**
+                 * 退出
+                 */
                 quit(): void;
+                /**
+                 * 强行退出
+                 */
                 exit(): void;
             }
             interface DialogAPI {
+                /**
+                 * 显示一个询问对话框，等待用户选择 "确定" 或 "取消" 按钮
+                 * @param message 消息
+                 * @returns 用户是否点击了 "确定" 按钮
+                 */
                 confirm(message?: string): Promise<boolean>;
+                /**
+                 * 显示一则消息
+                 * @param message 消息
+                 * @returns
+                 */
                 alert(message?: string): Promise<void>;
+                /**
+                 * 显示消息框
+                 * @param options
+                 * @returns
+                 */
                 messageBox(options: Electron.MessageBoxOptions): Promise<Electron.MessageBoxReturnValue>;
+                /**
+                 * 显示打开文件对话框
+                 * @param options
+                 * @returns
+                 */
                 openDialog(options: Electron.OpenDialogOptions): Promise<Electron.OpenDialogReturnValue>;
+                /**
+                 * 显示保存文件对话框
+                 * @param options
+                 * @returns
+                 */
                 saveDialog(options: Electron.SaveDialogOptions): Promise<Electron.SaveDialogReturnValue>;
             }
         }
