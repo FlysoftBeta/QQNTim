455c2b34cd36e9c376d590f3dcfef77b985cde0f
Flysoft <flysoftbeta@outlook.com>
2023-07-18 20:06:37 +0800

    fix(common/utils/ntVersion): unsupported platform error on macOS, closes https://github.com/Flysoft-Studio/QQNTim/issues/179 (#182)

--- src/common/utils/ntVersion.ts
+++ src/common/utils/ntVersion.ts
@@ -5,7 +5,7 @@ export function getCurrentNTVersion() {
     let version: string;
     if (process.platform == "win32") {
         version = readJSONSync(`${__dirname}${s}..${s}versions${s}config.json`)?.curVersion;
-    } else if (process.platform == "linux") {
+    } else if (process.platform == "linux" || process.platform == "darwin") {
         version = readJSONSync(`${__dirname}${s}..${s}package.json`)?.version;
     } else throw new Error(`unsupported platform: ${process.platform}`);
     if (!version) throw new Error("cannot determine QQNT version");
